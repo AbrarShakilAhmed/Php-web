a219981a1ff88dfe45fee6768efa893d57a672f0
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-04-05 21:55:31 +0600

    done

--- app.php
+++ app.php
@@ -2,6 +2,7 @@
 
 function para($text,$color="red"){
   echo "<p style='color:$color;font-size:42px;'>$text</p>";
+  echo "<p style='color:$color;font-size:42px;'>$text</p>";
 }
 
 $temp = " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut esse, soluta velit veniam facilis nostrum ";
