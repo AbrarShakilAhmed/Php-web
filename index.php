95584bc951e76e67a85b3608870c629af6ac3ce4
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-04-05 19:11:31 +0600

    doing

--- index.php
+++ index.php
@@ -1,18 +1,49 @@
 <?php
 
-$arr = [["lion",23,23000],
-          ["Sujon",34,34000],
-          ["sadin",56,42353],
-          ["manik",23,234234]];
+// $arr = [["lion",23,23000],
+//           ["Sujon",34,34000],
+//           ["sadin",56,42353],
+//           ["manik",23,234234]];
 
-$refineArr = [];
+// $refineArr = [];
 
 
-foreach($arr as $single){
-  if($single[1]<100){
-    array_push($refineArr,$single);
+// foreach($arr as $single){
+//   if($single[1]<100){
+//     array_push($refineArr,$single);
+//   }
+// }
+
+// print_r($refineArr);
+
+
+
+// fizzzBuzz pblm in php
+
+// div 3 = fizz
+// div 5 = fizzz
+// both = fizzzBuzz
+
+
+
+for($i=1;$i<=100;$i++){
+  if($i%15==0){
+    echo "fizzBuzz";
+    echo "<br>";
+  }elseif($i%3==0){
+    echo "fizz";
+    echo "<br>";
+
+  }elseif($i%5==0){
+    echo "Buzz";
+    echo "<br>";
+
+  }else{
+    echo $i;
+    echo "<br>";
+
+
   }
 }
 
-print_r($refineArr);
 ?>
