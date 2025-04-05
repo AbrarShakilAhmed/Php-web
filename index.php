aedb91382c49470004ce4527e2e2d6c39b65b8d4
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-04-05 19:06:22 +0600

    doing

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
 
-for($i = 1; $i<=10;$i++){
-for($j =1; $j<=$i;$j++){
-    echo $j;
+$arr = [["lion",23,23000],
+          ["Sujon",34,34000],
+          ["sadin",56,42353],
+          ["manik",23,234234]];
 
+$refineArr = [];
+
+
+foreach($arr as $single){
+  if($single[1]<100){
+    array_push($refineArr,$single);
+  }
 }
-  echo "<br>";
-  echo "<br>";
-}
+
+print_r($refineArr);
 ?>
