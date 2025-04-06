c9445cdbdd032b7c11800798952cb8408d4e1812
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-04-06 10:49:31 +0600

    doing

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-function app() {
-  return '546f'
-}
+let arr = [13, 35, 3253, 53, 453, 5, 34, 5, 34, 5]
 
-let news = app
+arr.forEach((e, i) => (arr[i] = Math.round(e / 2)))
 
-console.log(news())
+console.log(arr)
--- app.php
+++ app.php
@@ -1,13 +1,24 @@
 <?php
 
-function para($text,$color="red"){
-  echo "<p style='color:$color;font-size:42px;'>$text</p>";
-  echo "<p style='color:$color;font-size:42px;'>$text</p>";
+// function para($text,$color="red"){
+//   echo "<p style='color:$color;font-size:42px;'>$text</p>";
+//   echo "<p style='color:$color;font-size:42px;'>$text</p>";
+//   echo argu
+// }
+
+// $temp = " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut esse, soluta velit veniam facilis nostrum ";
+
+// para($temp,"red");
+
+function app(...$args) {
+    // Print the arguments passed to the function
+    print_r($args);
 }
 
-$temp = " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut esse, soluta velit veniam facilis nostrum ";
+$news = 'app';
 
-para($temp,"red");
+echo $news(23, 34, 234, 2); // This will call the function and print the arguments
+
 
 ?>
 
