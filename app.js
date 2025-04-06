64399e9a80c7c1a00171be8561bae20d8ef6fbdb
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-04-06 11:02:03 +0600

    doing

--- app.js
+++ app.js
@@ -3,3 +3,32 @@ let arr = [13, 35, 3253, 53, 453, 5, 34, 5, 34, 5]
 arr.forEach((e, i) => (arr[i] = Math.round(e / 2)))
 
 console.log(arr)
+
+let obj = [
+  { name: 'abrar', age: 34 },
+  { name: 'abrar', age: 34 },
+  { name: 'abrar', age: 34 },
+  { name: 'abrar', age: 34 },
+  { name: 'abrar', age: 34 },
+]
+
+obj.forEach((e) => {
+  return (e.age = e.age / 2)
+})
+
+console.log(obj)
+
+let newObj = obj.map((e) => {
+  return {
+    ...e,
+    age: e.age / 2,
+  }
+})
+
+console.log(newObj)
+
+const app = ['A', ' ', 'B']
+
+const sen = app.reduce((acc, i) => acc + i)
+
+console.log(sen)
