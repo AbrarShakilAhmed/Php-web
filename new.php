a2dbe7a65040b228cbbcf28e60a20b126073ff50
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-04-08 11:53:06 +0600

    doing

--- new.php
+++ new.php
@@ -1,3 +1,16 @@
 <?php
 
-echo phpinfo();
+function One(){
+  echo "working 1 function";
+}
+
+$test = 'One';
+
+function Two(){
+  global $test;
+  $test();
+}
+
+echo strlen("sjdhadhasdhahsd");
+
+// Two();
