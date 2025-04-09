3de83f16c360bdaf0cb6045d6e84f0f75d5197ac
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-04-09 21:47:33 +0600

    doing

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-// let str = 'hello world'
+let str = 'hello world'
 
-// function replaceword(vars, word, news) {
-//   if (vars.includes(word)) {
-//     return vars.replace(word, news)
-//   }
-//   return 0
-// }
+function replaceword(vars, word, news) {
+  if (vars.includes(word)) {
+    return vars.replace(word, news)
+  }
+  return 0
+}
 
-// console.log(replaceword(str, 'world', 'nes'))
+console.log(replaceword(str, 'world', 'nes'))
 
-// function replaceword2(vars, word, news) {
-//   vars = vars.split(' ')
-//   let newarr = []
-//   vars.forEach((element) => {
-//     if (element == word) {
-//       newarr.push(news)
-//     } else {
-//       newarr.push(element)
-//     }
-//   })
-//   return newarr.join(' ')
-// }
+function replaceword2(vars, word, news) {
+  vars = vars.split(' ')
+  let newarr = []
+  vars.forEach((element) => {
+    if (element == word) {
+      newarr.push(news)
+    } else {
+      newarr.push(element)
+    }
+  })
+  return newarr.join(' ')
+}
 
-// console.log(replaceword2(str, 'world', 'nes'))
-// clearImmediate()
+console.log(replaceword2(str, 'world', 'nes'))
+clearImmediate()
 
-// Create a Set
+Create a Set
 const letters = Object.values({ a: 'a', x: 'b', d: 'c' })
 
 console.table(letters.entries()) // List all Elements
