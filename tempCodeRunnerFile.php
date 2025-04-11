4af28ff9415ac940b2f5f465b6076255345feb0f
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-04-11 11:09:25 +0600

    doing

--- tempCodeRunnerFile.php
+++ tempCodeRunnerFile.php
@@ -1,2 +1,15 @@
 <?php
-Two();
+echo "<table border='1px'>";
+echo "<th>Name</th>";
+echo "<th>Age</th>";
+echo "<th>Position</th>";
+echo "<th>Salary</th>";
+foreach($UserData as $user){
+echo "<tr>";
+foreach($user as $single){
+  echo "<td>$single</td>";
+}
+echo "</tr>";
+
+}
+echo "</table>";
