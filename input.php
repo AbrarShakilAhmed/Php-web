231fcc3fc1eb29bcddf6ca8b6fa34a8b85c315a3
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-04-11 11:31:36 +0600

    doing

---
+++ input.php
@@ -0,0 +1,24 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+  <form action="./todo.php" method="get">
+    <input type="text" name="get">
+    <input type="submit" value="sub">
+  </form>
+  <br>
+  <form action="./todo.php" method="post">
+    <input type="text" name="post">
+    <input type="submit" value="sub">
+
+  </form>
+
+</body>
+</html>
