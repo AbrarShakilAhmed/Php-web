280427baff31ae5f518eb9d4a9e0461be3f4b6c8
AbrarShakilAhmed <skshakilppki123@gmail.com>
2025-04-23 12:39:39 +0600

    doing

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 
-// $host = "localhost";
-// $name = "root";
-// $pass = null;
-// $dbName = "college";
+$host = "localhost";
+$name = "root";
+$pass = null;
+$dbName = "college";
 
-// $conn = new mysqli($host,$name,$pass,$dbName);
-// $students = $conn->query("SELECT * FROM `students` WHERE 1")->fetch_all();
+$conn = new mysqli($host,$name,$pass,$dbName);
+$students = $conn->query("SELECT * FROM `students` WHERE 1")->fetch_all();
 
-// print_r($students[0]);
+print_r($students[0]);
 
 $dsn = "mysql:host=localhost;dbname=college";
 $username = "root";
